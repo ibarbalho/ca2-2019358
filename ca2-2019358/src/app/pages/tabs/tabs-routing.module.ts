2acc9e0c24bb3039084c06bf825ca25b3a35857e
Ittalo Barbalho <ittalo.renan@gmail.com>
2020-04-23 23:25:13 +0000

    adding tabs and icons

--- ca2-2019358/src/app/pages/tabs/tabs-routing.module.ts
+++ ca2-2019358/src/app/pages/tabs/tabs-routing.module.ts
@@ -68,7 +68,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
