c76ba0b1d3afd75d6a261ec08f3a49a38fa61d42
Ittalo Barbalho <ittalo.renan@gmail.com>
2020-05-30 16:21:27 +0000

    New tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  }
 ];
 
 @NgModule({
